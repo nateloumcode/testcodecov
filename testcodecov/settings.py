dbce81818502d9ca863cb3887831034e1b578f39
Nate Loum <nate.loum@propertymeld.com>
2023-05-10 18:58:32 -0500

    Get it hosting the react app

--- testcodecov/settings.py
+++ testcodecov/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'testcodecov.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'frontend/build']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
