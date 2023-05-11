a85a9a1ca3e516db83812ea56e939e4a6e6b6edc
Nate Loum <nate.loum@propertymeld.com>
2023-05-10 19:46:38 -0500

    Remove a test

--- testcodecov/calculator/tests/test_views.py
+++ testcodecov/calculator/tests/test_views.py
@@ -9,8 +9,3 @@ class Tests(TestCase):
         response = self.client.get(reverse('add'), data={'a': 1, 'b': 2})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, {'result': 3})
-
-    def test_divide(self):
-        response = self.client.get(reverse('divide'), data={'a': 10, 'b': 2})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {'result': 5})
