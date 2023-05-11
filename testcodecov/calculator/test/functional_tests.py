b2dd66a6d4402507369e43cea6570dc161cd449c
Nate Loum <nate.loum@propertymeld.com>
2023-05-10 19:00:24 -0500

    Fix functional test

--- testcodecov/calculator/test/functional_tests.py
+++ testcodecov/calculator/test/functional_tests.py
@@ -6,14 +6,13 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 class TestExample(StaticLiveServerTestCase):
     def test_example(self):
         with sync_playwright() as playwright:
-            browser = playwright.chromium.launch(headless=False)
+            browser = playwright.chromium.launch(headless=True)
             page = browser.new_context().new_page()
 
             # Navigate to your app
-            page.goto(self.live_server_url + "/index.html")
-            page.pause()
+            page.goto(self.live_server_url)
 
             # Perform some assertions or interactions with the page
-            self.assertIn("frontend", page.title())
+            self.assertIn("testcodecov", page.title())
 
             browser.close()
